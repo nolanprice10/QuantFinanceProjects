92a8c1bab9b3d8d897e2bfaa8f23c1a5c214b393
Nolan Price <nolangp10@gmail.com>
2026-09-11 19:24:56 +0000

    refactor: streamline option chain data retrieval and processing in IVsmile.py

--- IVsmile.py
+++ IVsmile.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from mpl_toolkits.mplot3d import Axes3D
 from datetime import datetime, date, time, timedelta
 import pandas as pd
@@ -9,24 +10,28 @@ ticker = yf.Ticker("SPY")
 period = "1y"
 expirations = ticker.options
 today = date.today()
+all_K = []
+all_maturity = []
+all_IV = []
 
-chain = ticker.option_chain()
-calls = chain.calls
-puts = chain.puts
-print("             Call Data:          ")
-print(calls[["strike", "impliedVolatility", "lastPrice"]])
-print("             Put Data:           ")
-print(puts[["strike", "impliedVolatility", "lastPrice"]])
-callK = calls["strike"]
-putK = puts["strike"]
-K = callK + putK
-callIV = calls["impliedVolatility"]
-putIV = puts["impliedVolatility"]
-IV = callIV + putIV
-maturity = np.array([ (datetime.strptime(exp, "%Y-%m-%d").date() - today).days/365.0 for exp in expirations ])
-x = K
-y = maturity
-z = IV
+for exp in expirations:
+    expiry_date = datetime.strptime(exp, "%Y-%m-%d").date()
+    days_to_maturity = (expiry_date - today).days
+    maturity_frac = days_to_maturity / 365.0
+
+    chain = ticker.option_chain(exp)
+    calls = chain.calls
+    current_K = list(calls["strike"])
+    current_IV = list(calls["impliedVolatility"])
+    num_K = len(current_K)
+    repeated_maturity = [maturity_frac] * num_K
+    all_K.extend(current_K)
+    all_maturity.extend(repeated_maturity)
+    all_IV.extend(current_IV)
+
+x = all_K
+y = all_maturity
+z = all_IV
 
 plt.figure()
 ax = plt.axes(projection='3d')
