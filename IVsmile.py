ed269b37c0fca495fee0f634ca296d006bead057
Nolan Price <nolangp10@gmail.com>
2026-09-11 17:26:01 +0000

    fix: correct option chain retrieval in IVsmile.py

--- IVsmile.py
+++ IVsmile.py
@@ -10,7 +10,7 @@ period = "1y"
 expirations = ticker.options
 today = date.today()
 
-chain = ticker.option_chain(today)
+chain = ticker.option_chain()
 calls = chain.calls
 puts = chain.puts
 print("             Call Data:          ")
@@ -27,3 +27,13 @@ maturity = np.array([ (datetime.strptime(exp, "%Y-%m-%d").date() - today).days/3
 x = K
 y = maturity
 z = IV
+
+plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter(x, y, z)
+ax.set_xlabel('Strike Price')
+ax.set_ylabel('Time to Maturity (Years)')
+ax.set_zlabel('Implied Volatility')
+plt.title("IV Smile")
+plt.show()
+plt.savefig('IV_smile.png')
