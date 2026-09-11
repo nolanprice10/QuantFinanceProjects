4d0f30f21f016163919f353a55d94d931a6655b1
Nolan Price <nolangp10@gmail.com>
2026-09-11 17:23:26 +0000

    Refactor code structure for improved readability and maintainability

--- IVsmile.py
+++ IVsmile.py
@@ -2,17 +2,28 @@ import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from datetime import datetime, date
+from datetime import datetime, date, time, timedelta
 import pandas as pd
 
 ticker = yf.Ticker("SPY")
 period = "1y"
 expirations = ticker.options
+today = date.today()
 
-chain = ticker.option_chain(expirations[0])
+chain = ticker.option_chain(today)
 calls = chain.calls
 puts = chain.puts
 print("             Call Data:          ")
 print(calls[["strike", "impliedVolatility", "lastPrice"]])
 print("             Put Data:           ")
 print(puts[["strike", "impliedVolatility", "lastPrice"]])
+callK = calls["strike"]
+putK = puts["strike"]
+K = callK + putK
+callIV = calls["impliedVolatility"]
+putIV = puts["impliedVolatility"]
+IV = callIV + putIV
+maturity = np.array([ (datetime.strptime(exp, "%Y-%m-%d").date() - today).days/365.0 for exp in expirations ])
+x = K
+y = maturity
+z = IV
